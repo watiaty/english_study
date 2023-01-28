77f17d19fd1cdec3526fca324abfc293ba456121
watiaty <yauheni.haikou@gmail.com>
2023-01-28 02:11:10 +0100

    Init commit

---
+++ src/main/java/com/watiaty/english_study/EnglishStudyApplication.java
@@ -0,0 +1,14 @@
+package com.watiaty.english_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableJpaAuditing
+public class EnglishStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/watiaty/english_study/EnglishStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watiaty.english_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
